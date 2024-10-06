fb656668987937f6d2359f853939ece6688e0551
Yoshiaki Kawazu <kawazzz@gmail.com>
2024-10-06 18:41:16 +0900

    lint: bunx @biomejs/biome check . --write

--- src/createWorker.ts
+++ src/createWorker.ts
@@ -1,60 +1,60 @@
 export interface SharedWorkerOptions extends WorkerOptions {
-	sameSiteCookies?: "all" | "none";
+	sameSiteCookies?: 'all' | 'none'
 }
 
 export const createWorkerWithDataUrl = (
 	f: () => void,
 	options?: WorkerOptions,
-) => new Worker(functionToDataUrl(f), options);
+) => new Worker(functionToDataUrl(f), options)
 
 export const createSharedWorkerWithDataUrl = (
 	f: () => void,
 	options?: SharedWorkerOptions,
-) => new SharedWorker(functionToDataUrl(f), options);
+) => new SharedWorker(functionToDataUrl(f), options)
 
 export const createWorkerWithBlob = (
 	f: () => void,
 	options?: WorkerOptions,
 ) => {
-	const url = functionToObjectUrl(f);
-	const w = new Worker(url, options);
-	URL.revokeObjectURL(url);
-	return w;
-};
+	const url = functionToObjectUrl(f)
+	const w = new Worker(url, options)
+	URL.revokeObjectURL(url)
+	return w
+}
 
 export const createSharedWorkerWithBlob = (
 	f: () => void,
 	options?: SharedWorkerOptions,
 ) => {
-	const url = functionToObjectUrl(f);
-	const w = new SharedWorker(url, options);
-	URL.revokeObjectURL(url);
-	return w;
-};
+	const url = functionToObjectUrl(f)
+	const w = new SharedWorker(url, options)
+	URL.revokeObjectURL(url)
+	return w
+}
 
 export const functionToDataUrl = (f: () => void) =>
-	`data:text/javascript;base64,${btoa(String.fromCharCode(...new TextEncoder().encode(`(${f.toString()})()`)))}`;
+	`data:text/javascript;base64,${btoa(String.fromCharCode(...new TextEncoder().encode(`(${f.toString()})()`)))}`
 
 export const functionToObjectUrl = (f: () => void) =>
 	URL.createObjectURL(
-		new Blob([`(${f.toString()})()`], { type: "text/javascript" }),
-	);
+		new Blob([`(${f.toString()})()`], { type: 'text/javascript' }),
+	)
 
 export const createWorker = (f: () => void, options?: WorkerOptions) => {
 	try {
-		return createWorkerWithDataUrl(f, options);
+		return createWorkerWithDataUrl(f, options)
 	} catch (e) {
-		return createWorkerWithBlob(f, options);
+		return createWorkerWithBlob(f, options)
 	}
-};
+}
 
 export const createSharedWorker = (
 	f: () => void,
 	options?: SharedWorkerOptions,
 ) => {
 	try {
-		return createSharedWorkerWithDataUrl(f, options);
+		return createSharedWorkerWithDataUrl(f, options)
 	} catch (e) {
-		return createSharedWorkerWithBlob(f, options);
+		return createSharedWorkerWithBlob(f, options)
 	}
-};
+}
--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,81 @@
-import { createWorker } from "./createWorker";
+import { createWorker } from './createWorker'
 
 // メインスレッド側で利用するインターフェース（）
-export type setTimeout = (func: TimerHandler, duration: number, ...args: unknown[]) => TimerId;
-export type setInterval = (func: TimerHandler, duration: number, ...args: unknown[]) => TimerId;
-export type clearTimeout = (id: TimerId) => void;
-export type clearInterval = (id: TimerId) => void;
+export type setTimeout = (
+	func: TimerHandler,
+	duration: number,
+	...args: unknown[]
+) => TimerId
+export type setInterval = (
+	func: TimerHandler,
+	duration: number,
+	...args: unknown[]
+) => TimerId
+export type clearTimeout = (id: TimerId) => void
+export type clearInterval = (id: TimerId) => void
 // 暗黙的なワーカースレッドを終了する
-export type terminate = () => void;
+export type terminate = () => void
 // ワーカースレッドを利用したタイマーオブジェクトを明示的に管理したい場合に使う
-export type createWorkerTimer = () => WorkerTimer;
+export type createWorkerTimer = () => WorkerTimer
 // ワーカースレッドを利用したタイマーオブジェクトのインターフェース
 export type WorkerTimer = {
-	setTimeout: setTimeout;
-	setInterval: setInterval;
-	clearTimeout: clearTimeout;
-	clearInterval: clearInterval;
-	terminate: terminate;
-};
+	setTimeout: setTimeout
+	setInterval: setInterval
+	clearTimeout: clearTimeout
+	clearInterval: clearInterval
+	terminate: terminate
+}
 // setTimeout/setInterval で使うIDは独自のユニークIDを利用する。
-type TimerId = number;
+type TimerId = number
 // setTimeout/setInterval のコールバック関数
-type TimerHandler = (...args: unknown[]) => void;
+type TimerHandler = (...args: unknown[]) => void
 
 // メインスレッドとワーカースレッド間で送受信するメッセージの型
-type MessageToWorker = SetTimerMessage | ClearTimerMessage;
-type MessageFromWorker = InvokeMessage | ClearMessage;
+type MessageToWorker = SetTimerMessage | ClearTimerMessage
+type MessageFromWorker = InvokeMessage | ClearMessage
 // ワーカーに送信するメッセージの型
 type SetTimerMessage = {
-	id: TimerId;
-	type: "setTimeout" | "setInterval";
-	duration: number;
-};
+	id: TimerId
+	type: 'setTimeout' | 'setInterval'
+	duration: number
+}
 type ClearTimerMessage = {
-	id: TimerId;
-	type: "clearTimeout" | "clearInterval";
-};
+	id: TimerId
+	type: 'clearTimeout' | 'clearInterval'
+}
 // ワーカーからメインスレッドに送信するメッセージの型
 type InvokeMessage = {
-	id: TimerId;
-	type: "invoke";
-};
+	id: TimerId
+	type: 'invoke'
+}
 type ClearMessage = {
-	id: TimerId;
-	type: "clear";
-};
+	id: TimerId
+	type: 'clear'
+}
 
 // デフォルトの暗黙的なWorkerTimerを保持する変数
-let defaultWorkerTimer: WorkerTimer | null = null;
+let defaultWorkerTimer: WorkerTimer | null = null
 // デフォルトの暗黙的なWorkerTimerを取得する
 const getWorkerTimer = (): WorkerTimer => {
 	if (defaultWorkerTimer == null) {
-		defaultWorkerTimer = createWorkerTimer();
+		defaultWorkerTimer = createWorkerTimer()
 	}
-	return defaultWorkerTimer;
-};
+	return defaultWorkerTimer
+}
 // デフォルトのWorkerTimerを利用してsetIntervalを呼び出す
-export const setInterval = (...args: Parameters<setInterval>) => getWorkerTimer().setInterval(...args);
+export const setInterval = (...args: Parameters<setInterval>) =>
+	getWorkerTimer().setInterval(...args)
 // デフォルトのWorkerTimerを利用してsetTimeoutを呼び出す
-export const setTimeout = (...args: Parameters<setTimeout>) => getWorkerTimer().setTimeout(...args);
+export const setTimeout = (...args: Parameters<setTimeout>) =>
+	getWorkerTimer().setTimeout(...args)
 // デフォルトのWorkerTimerを利用してclearIntervalを呼び出す
-export const clearInterval = (...args: Parameters<clearInterval>) => getWorkerTimer().clearInterval(...args);
+export const clearInterval = (...args: Parameters<clearInterval>) =>
+	getWorkerTimer().clearInterval(...args)
 // デフォルトのWorkerTimerを利用してclearTimeoutを呼び出す
-export const clearTimeout = (...args: Parameters<clearTimeout>) => getWorkerTimer().clearTimeout(...args);
+export const clearTimeout = (...args: Parameters<clearTimeout>) =>
+	getWorkerTimer().clearTimeout(...args)
 // デフォルトのWorkerTimerを利用してterminateを呼び出す
-export const terminate = () => getWorkerTimer().terminate();
+export const terminate = () => getWorkerTimer().terminate()
 
 // WorkerTimerを明示的に作成する（デフォルトのWorkerTimerを利用する場合はこの関数は使わない）
 // 以下のようなメッセージのやり取りで実装する
@@ -75,75 +87,88 @@ export const terminate = () => getWorkerTimer().terminate();
 export const createWorkerTimer = (): WorkerTimer => {
 	// メインスレッド側で保持するデータ
 	type TimerTransaction = {
-		id: TimerId;
-		type: "setTimeout" | "setInterval";
-		func: TimerHandler;
-		duration: number;
-		args: unknown[];
-	};
+		id: TimerId
+		type: 'setTimeout' | 'setInterval'
+		func: TimerHandler
+		duration: number
+		args: unknown[]
+	}
 	// setTimeout/setInterval で使うIDは独自のユニークIDを利用する。
-	const idMap: Map<TimerId, TimerTransaction> = new Map();
+	const idMap: Map<TimerId, TimerTransaction> = new Map()
 	// ユニークなタイマーIDを生成する関数
 	const nextId = (() => {
-		let seq = 0;
-		return () => seq++;
-	})();
+		let seq = 0
+		return () => seq++
+	})()
 	// ワーカースレッドを作成する
-	const worker = createWorker(timerWorker, { type: "module" });
+	const worker = createWorker(timerWorker, { type: 'module' })
 	// ワーカーからは invoke メッセージが来くるのでidMapからTimerHandlerを取り出して実行する
 	worker.addEventListener(
-		"message",
+		'message',
 		(message: MessageEvent<MessageFromWorker>) => {
-			const { id, type } = message.data;
-			const transaction = idMap.get(id);
+			const { id, type } = message.data
+			const transaction = idMap.get(id)
 			if (transaction == null) {
-				return;
+				return
 			}
-			if (type === "invoke") {
-				transaction.func(...transaction.args);
-			} else if (type === "clear") {
-				idMap.delete(id);
+			if (type === 'invoke') {
+				transaction.func(...transaction.args)
+			} else if (type === 'clear') {
+				idMap.delete(id)
 			}
 		},
-	);
+	)
 	// setTimeout/setInterval は同じインターフェースを持つので共通関数にしておく
-	const setTimer = (type: "setTimeout" | "setInterval", func: TimerHandler, duration: number, ...args: unknown[]) => {
-		const id = nextId();
-		idMap.set(id, { id, type, func, duration, args });
-		worker.postMessage({ id, type, duration } as SetTimerMessage);
-		return id;
-	};
+	const setTimer = (
+		type: 'setTimeout' | 'setInterval',
+		func: TimerHandler,
+		duration: number,
+		...args: unknown[]
+	) => {
+		const id = nextId()
+		idMap.set(id, { id, type, func, duration, args })
+		worker.postMessage({ id, type, duration } as SetTimerMessage)
+		return id
+	}
 	// clearTimeout/clearInterval は同じインターフェースを持つので共通関数にしておく
-	const clearTimer = (id: TimerId, type: "clearTimeout" | "clearInterval") => {
-		worker.postMessage({ id, type } as ClearTimerMessage);
-		idMap.delete(id);
-	};
+	const clearTimer = (id: TimerId, type: 'clearTimeout' | 'clearInterval') => {
+		worker.postMessage({ id, type } as ClearTimerMessage)
+		idMap.delete(id)
+	}
 	//
 	return {
-		setTimeout: (func: TimerHandler, duration: number, ...args: unknown[]) => setTimer("setTimeout", func, duration, ...args),
-		setInterval: (func: TimerHandler, duration: number, ...args: unknown[]) => setTimer("setInterval", func, duration, ...args),
-		clearTimeout: (id: TimerId) => clearTimer(id, "clearTimeout"),
-		clearInterval: (id: TimerId) => clearTimer(id, "clearInterval"),
+		setTimeout: (func: TimerHandler, duration: number, ...args: unknown[]) =>
+			setTimer('setTimeout', func, duration, ...args),
+		setInterval: (func: TimerHandler, duration: number, ...args: unknown[]) =>
+			setTimer('setInterval', func, duration, ...args),
+		clearTimeout: (id: TimerId) => clearTimer(id, 'clearTimeout'),
+		clearInterval: (id: TimerId) => clearTimer(id, 'clearInterval'),
 		terminate: () => worker.terminate(),
-	};
-};
+	}
+}
 
 // timerManagerで動作させるworkerコード
 const timerWorker = () => {
-	const idMap = new Map<TimerId, number>();
-	self.addEventListener("message", (event: MessageEvent<MessageToWorker>) => {
-		const { id, type } = event.data;
-		if (type === "setTimeout" || type === "setInterval") {
-			const setTimer = self[type];
-			const invoke = () => self.postMessage({ id, type: "invoke" });
-			const callback = type === "setTimeout" ? ()=>{invoke(); idMap.delete(id)} : invoke;
-			const nativeId = setTimer(callback, event.data.duration);
-			idMap.set(id, nativeId);
+	const idMap = new Map<TimerId, number>()
+	self.addEventListener('message', (event: MessageEvent<MessageToWorker>) => {
+		const { id, type } = event.data
+		if (type === 'setTimeout' || type === 'setInterval') {
+			const setTimer = self[type]
+			const invoke = () => self.postMessage({ id, type: 'invoke' })
+			const callback =
+				type === 'setTimeout'
+					? () => {
+							invoke()
+							idMap.delete(id)
+						}
+					: invoke
+			const nativeId = setTimer(callback, event.data.duration)
+			idMap.set(id, nativeId)
 		}
-		if (type === "clearTimeout" || type === "clearInterval") {
-			const clearTimer = self[type];
-			clearTimer(idMap.get(id));
-			idMap.delete(id);
+		if (type === 'clearTimeout' || type === 'clearInterval') {
+			const clearTimer = self[type]
+			clearTimer(idMap.get(id))
+			idMap.delete(id)
 		}
-	});
-};
+	})
+}
