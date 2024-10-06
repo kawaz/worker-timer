d0de59c494a7c9faa82a68cc56a4b132f2800d5e
Yoshiaki Kawazu <kawazzz@gmail.com>
2024-10-06 20:24:41 +0900

    add example

---
+++ example/index.ts
@@ -0,0 +1,15 @@
+import { clearInterval, setInterval, setTimeout } from '../src'
+
+// import { createWorkerTimer } from '../src'
+// const manager = createWorkerTimer()
+// const id = manager.setInterval(() => {
+// 	console.log('interval')
+// }, 1000)
+
+const id = setInterval(() => {
+	console.log('interval')
+}, 1000)
+
+const id2 = setTimeout(() => {
+	clearInterval(id)
+}, 5000)
